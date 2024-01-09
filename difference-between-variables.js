8b3fdad69de0ac74e20708228ccb176a2b553926
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2024-01-08 23:33:46 -0300

    Exemplo de tipos de variavel
    Aqui temos 2 exemplos a serem pontuados a diferença entre os tipo de variaveis e  quando temos um função declarada e n

---
+++ difference-between-variables.js
@@ -0,0 +1,21 @@
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+
+// function imprimirNumero(){
+//     var respostaDeTudo = 3.14
+//     let idade = 42
+//     const pi = 29
+//     console.log(respostaDeTudo, idade, pi)
+
+// }
+// imprimirNumero();
+console.log(respostaDeTudo, idade, pi)
